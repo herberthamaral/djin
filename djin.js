e1786866121ceeada56e7db273fcd9c423c24a9d
Herberth Amaral <herberthamaral@gmail.com>
2010-04-30 01:37:49 -0300

    Adding basic create database,create table and insert record support. TODO: improve error handling;

--- djin.js
+++ djin.js
@@ -1,7 +1,8 @@
+var DjinErrors ={}; //improve this.
 var Djin = (
   function()
   {
-  
+    
     create = function(databaseName)
     {
       var dbSize = 5*1024*1024; //5MB of default storage
@@ -11,6 +12,49 @@ var Djin = (
       return true;
     }
 
-    return {'create':create};
+    createTable = function(tableName,params) {
+      if(!this.dbPointer)
+        throw 'Can\'t access database object';
+      
+      this.dbPointer.transaction(function(t){
+        var fields = [];
+
+        for (p in params)
+        {
+          if(params[p]=='pk') //shortcut for primary key
+            fields.push(p+' INTEGER PRIMARY KEY');
+          else
+            fields.push(p+' '+params[p]);
+        }
+        t.executeSql('CREATE TABLE IF NOT EXISTS '+tableName+'('+fields.join(',')+')',[],null,
+          function(e){ DjinErrors['createTable']='Unable to create a table'; }); 
+      });
+      return DjinErrors['createTable'] == null;
+    }
+
+    add = function(tableName,value)
+    {
+      if(!this.dbPointer)
+        throw 'Can\'t access database object';
+      
+      this.dbPointer.transaction(function(t){
+        var fields = [];
+        var values = [];
+        var nvalues = [];
+        for (i in value)
+        {
+          fields.push(i);
+          values.push(value[i]);
+          nvalues.push('?')
+        }
+        t.executeSql('INSERT INTO '+tableName+'('+fields.join(',')+') VALUES('+nvalues.join(',')+')',values,null,
+            function(e){ DjinErrors['insert']='Unable to insert item:'+e} );
+      });
+
+      return DjinErrors['insert']==null;
+    }
+
+    return {'create':create,'createTable':createTable,'add':add};
+
   }
 )(); 
--- djin_tests.js
+++ djin_tests.js
@@ -1,14 +1,21 @@
 allDjinTests = function()
 {
   //var djin = new Djin();
-
+  module('Data Definition Module');
   test("Initialize an empty Database",
       function(){
         ok(Djin.create("myDataBase"),"Create a new database with 5MB of storage (default)");
 
       });
-  test("Try to create a table in current database",
+
+  test("Try to create a table with only one field in current database",
     function(){
-      
-    })
+      ok(Djin.createTable('person',{'id':'pk'}));
+    });
+  
+  module('Data Manipulation Module');
+  test("Try to create a record in person's table",
+      function(){
+        ok(Djin.add('person',{'id':null}));
+      } );
 }
